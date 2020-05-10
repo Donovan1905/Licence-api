23941651ef63ef4228e6d643d1341441f21dc3cf
Donovan1905 <44325654+Donovan1905@users.noreply.github.com>
2020-05-10 15:08:23 +0200

    Implementation des endpoints de bases pour le model licence

--- app_licences/models.py
+++ app_licences/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-from app_companies import models
+from app_companies.models import Company
 
+
+class Licence(models.Model):
+    key = models.CharField(max_length=20)
+    company = models.OneToOneField(
+        'app_companies.Company',
+        on_delete=models.CASCADE
+    )
+    user_id = models.IntegerField(default=0, null=True)
+
+    def __str__(self):
+        return self.key
--- app_licences/views.py
+++ app_licences/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Licence
+from .serializers import LicenceSerializer
+from django.views.decorators.csrf import csrf_exempt
+from app_companies.models import Company
 
-# Create your views here.
+
+@csrf_exempt
+def licence_list(request):
+    if request.method == 'GET':
+        licences = Licence.objects.all()
+        serializer = LicenceSerializer(licences, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONPaser().parse(request)
+        serializer = LicenceSerializer(data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonReponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status = 400)
+
+
+@csrf_exempt
+def licence_details(request, pk):
+    try:
+        licence = Licence.objects.get(pk=pk)
+    except Licence.DoesNotExist:
+        return HttpResponse(status=400)
+
+    if request.method == 'GET':
+        serializer = LicenceSerializer(licence)
+        return JsonResponse(serialiser.data, safe=False)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = LicenceSerializer(licence, data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return HttpResponse(status=400)
+    elif request.method == 'DELETE':
+        company.delete()
+        return HttpResponse(status=204)
